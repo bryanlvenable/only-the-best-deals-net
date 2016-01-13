c55dae453149e55026a76528ad4f7d9e358219c8
Bryan L. Venable <bryanlvenable@gmail.com>
2016-01-12 20:07:53 -0800

    hello world served up

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ var express = require('express'),
 var app = express(),
     port = config.port,
     host = config.host;
+
+
+// Temp hello world routes
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port);
+console.log('The best deals on the net are being served at ' + host + port);
