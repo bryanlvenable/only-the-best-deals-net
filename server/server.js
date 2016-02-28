badabc9533c96638cfd3bd0f84ade34c017de29c
Bryan L. Venable <bryanlvenable@gmail.com>
2016-02-27 20:38:15 -0800

    served up hello world with express-handlebars

--- server/server.js
+++ server/server.js
@@ -2,18 +2,27 @@
 
 // Get all the packages
 var express = require('express'),
-    config  = require('./config/config');
+    config  = require('./config/config'),
+    exphbs = require('express-handlebars');
 
 var app = express(),
     router = express.Router(),
+    hbs = exphbs.create({/* config */}),
     port = config.port,
     host = config.host;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// Temp hello world routes
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function (req, res, next) {
+    res.render('splash');
 });
+
+// Temp
+// Temp hello world routes
+// app.get('/', function (req, res) {
+//     res.send('Hello World!');
+// });
 
 app.listen(port);
 console.log('The best deals on the net are being served at ' + host + port);
