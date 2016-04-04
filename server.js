34fcd8c2b28602c6887982650364833e823a4018
Bryan L. Venable <bryanlvenable@gmail.com>
2016-04-03 20:16:41 -0700

    boostrap working

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ var express = require('express'),
 
 var app = express(),
     router = express.Router(),
-    hbs = exphbs.create({/* config */}),
+    hbs = exphbs.create({
+        defaultLayout: 'main',
+    }),
     port = config.port,
     host = config.host;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use('/bootstrap', express.static('bower_components/bootstrap/dist'));
+app.use('/jquery', express.static('bower_components/jquery/dist'));
 
 app.get('/', function (req, res, next) {
     res.render('splash');
