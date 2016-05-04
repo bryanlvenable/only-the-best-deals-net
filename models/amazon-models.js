88402b0605e55c00f0f88e06a727206a14d9936f
Bryan L. Venable <bryanlvenable@gmail.com>
2016-05-03 23:16:15 -0700

    showing data and images

--- models/amazon-models.js
+++ models/amazon-models.js
@@ -15,21 +15,17 @@ Amazon.prototype.search = function(options, callback) {
     this.query = {
         Keywords: options.keywords,
         SearchIndex: "SportingGoods",
-        ResponseGroup: "Images"
+        ResponseGroup: "ItemAttributes, Images"
     };
     console.log(this.query);
 
     this.prodAdv.call("ItemSearch", this.query, function(err, result) {
         this.results = [];
-        console.log("result keys: ", Object.keys(result.Items.Item[0]));
-        // console.log("result keys: ", result.Items.TotalResults);
-
 
         result.Items.Item.forEach(function(item) {
-            // console.log("item.ItemAttributes.Image: ", item.ItemLinks);
             this.entry = {
-                // url: item.DetailPageURL,
-                // title: item.ItemAttributes.Title,
+                url: item.DetailPageURL,
+                title: item.ItemAttributes.Title,
                 image: item.MediumImage.URL
             };
             this.results.push(this.entry);
