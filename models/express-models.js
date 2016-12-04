5b0558ad766d39b19ed46eee2a6c6d853664a72e
Bryan L. Venable <bryanlvenable@gmail.com>
2016-12-03 19:01:37 -0800

    added better error handling

--- models/express-models.js
+++ models/express-models.js
@@ -5,10 +5,17 @@ var Express = function(config) {
 };
 
 Express.prototype.search = function(options, callback) {
+    const is = require('is_js');
     this.amazon = new this.Amazon(this.config.amazonAssociates);
     this.amazon.search(options, function(err, results) {
+        if (is.not.existy(results) || is.not.array(results)) {
+            return callback(new Error('results are not an array'));
+        }
+        if (is.not.existy(results[0].url) || is.not.array(results[0].url)) {
+            return callback(new Error('results[0].url are not an array'));
+        }
         // TODO - currently only returning the first result's url
-        callback(err, results[0].url[0]);
+        callback(null, results[0].url[0]);
     });
 };
 
